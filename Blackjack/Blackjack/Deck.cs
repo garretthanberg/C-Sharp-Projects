d15ff616f74082c647bc146ab989b4a6e5fa935b
garretthanberg <garretthanbergphoto@gmail.com>
2023-04-03 14:35:54 -0700

    Made changes to the blackjack program

--- Blackjack/Blackjack/Deck.cs
+++ Blackjack/Blackjack/Deck.cs
@@ -30,5 +30,22 @@ namespace Blackjack
             }
         }
         public List<Card> Cards { get; set; }
+
+        public void shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> tempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    tempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = tempList;
+            }
+        }
     }
 }
