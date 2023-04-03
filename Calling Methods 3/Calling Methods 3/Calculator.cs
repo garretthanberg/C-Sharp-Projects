251816e6cf09fd8ce5aba231121b4d30afd82dd4
garretthanberg <garretthanbergphoto@gmail.com>
2023-04-03 12:46:31 -0700

    Practiced calling methods a third time.

---
+++ Calling Methods 3/Calling Methods 3/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_3
+{
+    public class Calculator
+    {
+        public int mathOperation(int firstNumber, int secondNumber = 0)
+        {
+            // This will do a math operation and return an integer result.
+            int result = firstNumber + secondNumber;
+            return result;
+        }
+    }
+}
---
+++ Calling Methods 3/Calling Methods 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will instantiate the class.
+            Calculator calculator = new Calculator();
+
+            // This will ask the user to input two numbers, one at a time.
+            Console.WriteLine("Enter your first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number (optional): ");
+            string secondNumberString = Console.ReadLine();
+
+            // This calls the method in the class, passing in the one or two numbers entered.
+            if (string.IsNullOrEmpty(secondNumberString))
+            {
+                int result = calculator.mathOperation(firstNumber);
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                int secondNumber = int.Parse(secondNumberString);
+                int result = calculator.mathOperation(firstNumber, secondNumber);
+                Console.WriteLine("Result: " + result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
