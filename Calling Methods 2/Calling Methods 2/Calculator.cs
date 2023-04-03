a8cd600aaa0c899da2d9ae4d21c5a0090886e6be
garretthanberg <garretthanbergphoto@gmail.com>
2023-04-03 12:30:49 -0700

    Practiced calling methods more.

---
+++ Calling Methods 2/Calling Methods 2/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_2
+{
+    // This defines a class for math operations.
+    public class Calculator
+    {
+        // This method takes an integer, adds 10 to it, and returns the result.
+        public int mathOperation(int input)
+        {
+            return input + 10;
+        }
+
+        // This method takes a decimal, multiplies it by 2, and returns the result.
+        public int mathOperation(decimal input)
+        {
+            return (int)(input * 2);
+        }
+
+        // This method takes a string, tries to parse it as an integer, adds 5 to it, and returns the result.
+        public int mathOperation(string input)
+        {
+            int parsedInput;
+            if (int.TryParse(input, out parsedInput))
+            {
+                return parsedInput + 5;
+            }
+            else
+            {
+                Console.WriteLine("Input could not be parsed as an integer.");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Calling Methods 2/Calling Methods 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // This instantiates the Calculator class.
+            Calculator calculator = new Calculator();
+
+            // This calls the first method with an integer input and displays the result.
+            int intResult = calculator.mathOperation(5);
+            Console.WriteLine("Integer result: " + intResult);
+
+            // This calls the second method with a decimal input and displays the result.
+            int decimalResult = calculator.mathOperation(3.14m);
+            Console.WriteLine("Decimal result: " + decimalResult);
+
+            // This calls the third method with a string input and displays the result.
+            int stringResult = calculator.mathOperation("10");
+            Console.WriteLine("String result: " + stringResult);
+
+            // This waits for the user to press a key before closing the console.
+            Console.ReadLine();
+        }
+    }
+}
