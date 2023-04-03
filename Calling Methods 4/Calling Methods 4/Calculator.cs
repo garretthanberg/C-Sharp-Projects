3658ccdd027549dfd3f17ca2cc0c3699c0853199
garretthanberg <garretthanbergphoto@gmail.com>
2023-04-03 12:57:26 -0700

    Practiced calling methods a 4th time.

---
+++ Calling Methods 4/Calling Methods 4/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_4
+{
+    public class Calculator
+    {
+        // This is a void method that takes two integers and performs a math operation on the first integer.
+        // This also displays the second integer in the console.
+        public void mathOperation(int x, int y)
+        {
+            int result = x * 2 + 10;
+            Console.WriteLine($"The second integer is: {y}");
+        }
+    }
+}
---
+++ Calling Methods 4/Calling Methods 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator(); // This instantiates the Calculator class.
+
+            // This calls the method with two arguments.
+            calculator.mathOperation(5, 7);
+
+            // This calls the method with named parameters.
+            calculator.mathOperation(x: 10, y: 20);
+
+            Console.ReadLine();
+        }
+    }
+}
