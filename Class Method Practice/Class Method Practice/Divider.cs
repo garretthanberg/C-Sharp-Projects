c3a10d0531c6290a9ee125452cf0c641fcbf9a65
garretthanberg <garretthanbergphoto@gmail.com>
2023-04-04 12:24:31 -0700

    Practiced with class methods

---
+++ Class Method Practice/Class Method Practice/Divider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Practice
+{
+    public class Divider
+    {
+        // This creates a void method that outputs an integer.
+        public void divideBy2(int num)
+        {
+            // This divides the data passed to it by 2.
+            int result = num / 2;
+            Console.WriteLine($"The result of dividing {num} by 2 is {result}");
+        }
+
+        // THis creates a method with output parameters.
+        public int multiplyBy2(int num, out int product)
+        {
+            product = num * 2;
+            return product;
+        }
+
+        // This overloads the method.
+        public int multiplyBy2(int num)
+        {
+            return num * 2;
+        }
+    }
+}
---
+++ Class Method Practice/Class Method Practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Practice
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // This instantiates the Divider class();
+            Divider divider = new Divider();
+
+            // This has the user enter a number.
+            Console.WriteLine("Enter a number: ");
+
+            // This reads the number from the user input.
+            int num = int.Parse(Console.ReadLine());
+
+            // This calls the divideBy2 method on that number.
+            divider.divideBy2(num);
+
+            // This calls the multiplyBy2 method with output parameters.
+            int product;
+            int result1 = divider.multiplyBy2(num, out product);
+            Console.WriteLine($"The result of multiplying {num} by 2 is {result1}, and the product is {product}");
+
+            // This calls the overloaded multipltBy2 method.
+            int result2 = divider.multiplyBy2(num);
+            Console.WriteLine($"The result of overloading the method to multiply {num} by 2 is {result2}");
+
+            Console.ReadLine();
+        }
+    }
+}
