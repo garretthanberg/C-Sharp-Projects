d44e9864803090c6fa787a4e083d754887688fa2
garretthanberg <garretthanbergphoto@gmail.com>
2023-04-08 12:40:55 -0700

    Practiced with enums

---
+++ Enum Practice/Enum Practice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // This prompts the user to enter the current day of the week.
+                Console.WriteLine("Enter the current day of the week: ");
+                string input = Console.ReadLine();
+
+                // This parses the user input as an enum value and assigns it to a variable.
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), input, true);
+
+                // This prints the current day of the week to the console.
+                Console.WriteLine("Today is {0}.", currentDay);
+            }
+            catch (ArgumentException)
+            {
+                // This catches an ArgumentException that may occur due to invalid input.
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+    // This creates an enum for the days of the week.
+    enum DaysOfWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+    }
+}
