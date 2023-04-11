6d360b6cd5b55b8e17636017a19d009b358685be
garretthanberg <garretthanbergphoto@gmail.com>
2023-04-11 16:54:17 -0700

    Practiced with Date Time

--- Blackjack/Blackjack/Dealer.cs
+++ Blackjack/Blackjack/Dealer.cs
@@ -20,6 +20,7 @@ namespace Blackjack
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\garretthanberg\source\Logs\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
---
+++ Date Time Practice/Date Time Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_Time_Practice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // This gets the current date and time.
+            DateTime currentTime = DateTime.Now;
+
+            // This prints the current date and time to the console.
+            Console.WriteLine("Current Date and Time: " + currentTime);
+
+            // This asks the user for a number.
+            Console.WriteLine("Enter a number: ");
+            int hours = int.Parse(Console.ReadLine());
+
+            // This calculates the time X (The number entered) hours from now.
+            DateTime futureTime = currentTime.AddHours(hours);
+
+            // This prints the future time to the console.
+            Console.WriteLine("Future Time: " + futureTime);
+
+            Console.ReadLine();
+        }
+    }
+}
