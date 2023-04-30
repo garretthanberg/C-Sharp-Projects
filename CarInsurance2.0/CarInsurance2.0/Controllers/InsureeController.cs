7e8c36d093d9a4bf3997f916f4674a368dd2cd67
garretthanberg <garretthanbergphoto@gmail.com>
2023-04-30 16:05:34 -0700

    Created a car insurance web app

---
+++ CarInsurance2.0/CarInsurance2.0/Controllers/InsureeController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance2._0.Models;
+
+namespace CarInsurance2._0.Controllers
+{
+    public class InsureeController : Controller
+    {
+        private InsuranceEntities db = new InsuranceEntities();
+
+        // GET: Insuree
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+
+        // GET: Insuree/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Insurees.Add(insuree);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insuree/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(insuree).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(insuree);
+        }
+
+        // GET: Insuree/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Insuree insuree = db.Insurees.Find(id);
+            if (insuree == null)
+            {
+                return HttpNotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insuree/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Insuree insuree = db.Insurees.Find(id);
+            db.Insurees.Remove(insuree);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // GET: Insuree/Quote
+        public ActionResult Quote()
+        {
+            return View();
+        }
+
+        // POST: Insuree/Quote
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Quote([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            decimal monthlyTotal = 50;
+
+            // Age calculation.
+            int age = DateTime.Now.Year - insuree.DateOfBirth.Year;
+            if (age <= 18)
+            {
+                monthlyTotal += 100;
+            }
+            else if (age >= 19 && age <= 25)
+            {
+                monthlyTotal += 50;
+            }
+            else
+            {
+                monthlyTotal += 25;
+            }
+
+            // Car year calculation.
+            if (insuree.CarYear < 2000 || insuree.CarYear > 2015)
+            {
+                monthlyTotal += 25;
+            }
+
+            // Car make and model calculation.
+            if (insuree.CarMake.ToLower() == "porsche")
+            {
+                monthlyTotal += 25;
+
+                if (insuree.CarModel.ToLower() == "911 carrera")
+                {
+                    monthlyTotal += 25;
+                }
+            }
+
+            // Speeding tickets calculation.
+            monthlyTotal += insuree.SpeedingTickets * 10;
+
+            // DUI calculation.
+            if (insuree.DUI)
+            {
+                monthlyTotal *= 1.25m;
+            }
+
+            // Full coverage calculation.
+            if (insuree.CoverageType)
+            {
+                monthlyTotal *= 1.5m;
+            }
+
+            insuree.Quote = monthlyTotal;
+            return View(insuree);
+        }
+
+        public ActionResult Admin()
+        {
+            var insurees = db.Insurees.ToList();
+            return View(insurees);
+        }
+
+    }
+}
