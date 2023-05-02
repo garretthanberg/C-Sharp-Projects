5b6b8f284d4d386249b602206ef85427f0b2590e
garretthanberg <garretthanbergphoto@gmail.com>
2023-05-02 14:44:21 -0700

    Server Time Challenege

---
+++ Server Time Challenge/Server Time Challenge/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Server_Time_Challenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
